814f324c7c9e912082262fb014e77c3df8161c32
HrishikShaji <hrishikminishaji@gmail.com>
2024-04-13 19:32:18 +0530

    combined reducers

--- app/store.ts
+++ app/store.ts
@@ -1,58 +1,80 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
 
 type ActionType = {
-	type: string;
-	info: string;
+  type: string;
+  info: string;
 };
 
-type InitialState = {
-	noOfCakes: number;
-	noOfIcecreams: number;
+type InitialCakeState = {
+  noOfCakes: number;
+};
+type InitialIceCreamState = {
+  noOfIcecreams: number;
 };
 
-const initialState: InitialState = {
-	noOfCakes: 10,
-	noOfIcecreams: 15,
+const initialCakeState: InitialCakeState = {
+  noOfCakes: 10,
+};
+const initialIceCreamState: InitialIceCreamState = {
+  noOfIcecreams: 15,
 };
 
 export function buyCake() {
-	return {
-		type: BUY_CAKE,
-		info: "buy-cake",
-	};
+  return {
+    type: BUY_CAKE,
+    info: "buy-cake",
+  };
 }
 
 export function buyIceCream() {
-	return {
-		type: BUY_ICECREAM,
-		info: "buy-icecream",
-	};
+  return {
+    type: BUY_ICECREAM,
+    info: "buy-icecream",
+  };
 }
 
-function reducer(state: InitialState = initialState, action: ActionType) {
-	switch (action.type) {
-		case BUY_CAKE:
-			return {
-				...state,
-				noOfCakes: state.noOfCakes - 1,
-			};
-		case BUY_ICECREAM:
-			return {
-				...state,
-				noOfIcecreams: state.noOfIcecreams - 1,
-			};
+function cakeReducer(
+  state: InitialCakeState = initialCakeState,
+  action: ActionType,
+) {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        noOfCakes: state.noOfCakes - 1,
+      };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
+}
+function iceCreamReducer(
+  state: InitialIceCreamState = initialIceCreamState,
+  action: ActionType,
+) {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        noOfIcecreams: state.noOfIcecreams - 1,
+      };
+
+    default:
+      return state;
+  }
 }
 
-export const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+export const store = createStore(rootReducer);
 console.log("initial state", store.getState());
 
 const unsubscribe = store.subscribe(() =>
-	console.log("updated", store.getState()),
+  console.log("updated", store.getState()),
 );
